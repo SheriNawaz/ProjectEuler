e6e37fc3f0c46dfc83731b14a11bd903a7b7ab30
Sheri Nawaz <47926276+SheriNawaz@users.noreply.github.com>
2023-01-27 23:21:17 +0000

    Comments

--- 5 - Smallest Multiple/main.py
+++ 5 - Smallest Multiple/main.py
@@ -10,14 +10,15 @@ def main():
     num_found = False
     i = 20
     while not num_found:
+        #Check if i is divisible by every number from 1 to 20
         for j in range(1, 21):
-            if i % j != 0:
+            if i % j != 0: #If i is not divisible by any number, end the for loop
                 print(i)
                 break
-            if j == 20 and i % j == 0:
+            if j == 20 and i % j == 0: #If i is divisible by every number, print it out and end while loop
                 print(i)
                 num_found = True
-        i += 20
+        i += 20 #Increment by 20 to make program 20 times faster
 
 
 
--- 6 Sum Square Difference/main.py
+++ 6 Sum Square Difference/main.py
@@ -5,14 +5,19 @@ Find the difference between the sum of the squares of the first one hundred natu
 
 
 def main():
+    #Get sum of squares of all numbers between 1 and 100
+
     sum_of_squares = 0
     for i in range(1, 101):
         sum_of_squares += i ** 2
+
+    #Get square of sum of all numbers between 1 and 100
     sum = 0
     for i in range(1, 101):
         sum += i
     square_of_sum = sum ** 2
 
+    #Print out difference
     print(square_of_sum - sum_of_squares)
 
 
