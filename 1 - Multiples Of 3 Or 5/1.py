5c38d1795b5eb2ac067bf1505e6bcce136b418ad
Sheri Nawaz <47926276+SheriNawaz@users.noreply.github.com>
2023-01-27 18:34:41 +0000

    Overall Reformat

--- 1 - Multiples Of 3 Or 5/1.py
+++ 1 - Multiples Of 3 Or 5/1.py
@@ -1,11 +1,11 @@
-'''
+"""
 Task 1
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
-'''
+"""
 
 total = 0
 for i in range(1, 1000):
-    if i % 5 == 0 or i % 3 == 0:
-        total += i
+    if i % 5 == 0 or i % 3 == 0:  # If number is a multiple of 3 or 5
+        total += i  # Increase the total by i
 print(total)
--- 3 - Largest Prime Factor/main.py
+++ 3 - Largest Prime Factor/main.py
@@ -1,18 +1,29 @@
+"""
+Task 3
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143?
+"""
+
+
 import math
+
 
 def main():
     number = 600851475143
     primes = []
     if number % 2 == 0:
         primes.append(2)
-    for i in range(3, math.ceil(number**0.5), 2):
+    """ Loop through every odd number from 3 to the square root of number (as these are all of the possible numbers
+    that number can be divisible by) """
+    for i in range(3, math.ceil(number ** 0.5), 2):
         if is_prime(i) and (number % i) == 0:
             primes.append(i)
-    print(primes[len(primes) - 1])
+    print(primes[len(primes) - 1])  # Print last number of primes list
 
 
 def is_prime(n):
-    for i in range(2, math.ceil(n**0.5)):
+    for i in range(2, math.ceil(n ** 0.5)):
+        # If n is divisible by i then n isn't a prime number
         if (n % i) == 0:
             return False
     return True
